e20877185c9cac20ce235cc865de6b9817516bb9
Vasudev suthar <doyalvasudev87@gmail.com>
2024-02-24 19:16:07 -0800

    rechange all chenges

--- src/app.js
+++ src/app.js
@@ -3,11 +3,7 @@ import cors from "cors"
 
 const app = express()
 
-app.use(cors({
-    origin: '*',
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
-}))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
