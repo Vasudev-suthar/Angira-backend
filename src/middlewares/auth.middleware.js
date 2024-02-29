e49f0eecb97e99961373d3c83776668f44e14374
Vasudev suthar <doyalvasudev87@gmail.com>
2024-02-28 23:58:31 -0800

    add bearer in authentication

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,8 +3,12 @@ import { ApiError } from "../utils/ApiError.js";
 
 const authenticateToken = (req, res, next) => {
     try {
-        const token = req.headers.authorization;
+        const authorization = req.headers.authorization
+        if (!authorization) {
+            return res.status(401).json({ error: 'Token Not Found' });
+        }
 
+        const token = req.headers.authorization.split(' ')[1];
         if (!token) {
             return res.status(401).json({ message: 'Unauthorized' });
         }
